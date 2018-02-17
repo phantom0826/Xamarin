663ce9ad76ecabea0a6b596a46d17e7bbcd31f24
phantom0826 <phantom08266@gmail.com>
2018-02-17 13:18:37 +0900

    add

---
+++ TestProject/Xamarin_Forms_Test/App3/App3/App3/MainPage.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace App3
+{
+	public partial class MainPage : ContentPage
+	{
+		public MainPage()
+		{
+			InitializeComponent();
+            Button hellobtn = new Button
+            {
+                Text = "Navigate",
+                HorizontalOptions = LayoutOptions.Start,
+                VerticalOptions = LayoutOptions.Start
+            };
+            Button plusbtn = new Button
+            {
+                Text = "Plus",
+                HorizontalOptions = LayoutOptions.Center,
+                VerticalOptions = LayoutOptions.Center
+            };
+
+            hellobtn.Clicked += async (sender, args) =>
+            {
+                await Navigation.PushAsync(new HelloXamlPagexaml());
+            };
+            plusbtn.Clicked += async (sender, args) =>
+             {
+                 await Navigation.PushAsync(new XamlPlusCodePage());
+             };
+            Content = hellobtn;
+            Content = plusbtn;
+		}
+        
+    }
+}
---
+++ TestProject/Xamarin_Forms_Test/App3/App3/App3/XamlPlusCodePage.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace App3
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class XamlPlusCodePage : ContentPage
+	{
+		public XamlPlusCodePage ()
+		{
+			InitializeComponent ();
+		}
+        void OnSliderValueChanged(object sender, ValueChangedEventArgs args)
+        {
+            //valueLabel.Text = args.NewValue.ToString("F3");
+            valueLabel.Text = ((Slider)sender).Value.ToString("F3");
+        }
+
+        async void OnButtonClicked(object sender, EventArgs args)
+        {
+            Button button = (Button)sender;
+            await DisplayAlert("Clicked!", "The button labeled '" + button.Text + "' has been clicked", "OK");
+        }
+    }
+}
